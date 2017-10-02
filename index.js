8bc2618a93a228364bd0c3b8fcb635a6df0545ab
uykalkan <uykalkan@gmail.com>
2017-10-02 17:52:59 +0300

    fix

--- index.js
+++ index.js
@@ -6,24 +6,51 @@ var inquirer = require('inquirer');
 var program = require('commander');
 var os = require('os');
 
-var file = path.join(os.homedir(), '/.fatih.json');
+var setting_file = path.join(os.homedir(), '/.fatih.json');
 var path_here  = shell.pwd().stdout;
 var args = process.argv.slice(2);
 
 var exec = require('child_process').exec;
 
+var default_settings = {
+    "settings" : {
+        "editor" : "code",
+        "terminal" : "cmd"
+    }, "index" : {
+        
+    }
+}
+
 if (args[0]) {
     jsonfile.readFile('./fatih.json', function(err, obj) {
-        if (obj.custom_commands[args[0]]) {
-            var selected_command = obj.custom_commands[args[0]];
-            shell.exec('start cmd /K "'+selected_command+'"');
-            process.exit(1);
-        } else if (args[0] == 'is') {
-            shell.exec('start '+obj.github+'/issues');
-            process.exit(1);
-        }
+        try {
+            if (obj.custom_commands[args[0]]) {
+                console.log('asdasdasd');
+                var selected_command = obj.custom_commands[args[0]];
+                shell.exec('start cmd /K "'+selected_command+'"');
+                process.exit(1);
+            } else if (args[0] == 'is') {
+                shell.exec('start '+obj.github+'/issues');
+                process.exit(1);
+            }    
+        } catch(e){
+            if(e){
+                
+            }
+        }         
     })    
 }
+
+// function index(project_name, project_path) {
+//     jsonfile.readFile(setting_file, function(err, obj) {
+//         if (obj) {
+//             obj[project_name] = project_path;
+//             jsonfile.writeFileSync(setting_file, obj, {spaces: 2});
+//         } else {
+//             jsonfile.writeFileSync(setting_file, default_settings, {spaces: 2});
+//         }
+//     })
+// }
 
 program.version('2.0.0');
 
