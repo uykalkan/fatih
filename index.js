a66fe4eaa456fa9525668dbcf2b81fdca9c9093c
uykalkan <uykalkan@gmail.com>
2017-10-02 17:26:40 +0300

    v2 start

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 var jsonfile = require('jsonfile');
-var argv = require( 'argv' );
 var path = require('path');
 var shell = require('shelljs');
 var inquirer = require('inquirer');
@@ -9,186 +8,54 @@ var os = require('os');
 
 var file = path.join(os.homedir(), '/.fatih.json');
 var path_here  = shell.pwd().stdout;
+var args = process.argv.slice(2);
 
-config_default = {
-    "editor": "code",
-    "projects": {
-        "fatih": {
-          "location" : "C:\\xampp\\htdocs\\fatih"
-        },
-        "fastback": {
-          "location" : "C:\\xampp\\htdocs\\fastback"
-        }    
-    }
+var exec = require('child_process').exec;
+
+if (args[0]) {
+    jsonfile.readFile('./fatih.json', function(err, obj) {
+        if (obj.custom_commands[args[0]]) {
+            var selected_command = obj.custom_commands[args[0]];
+            shell.exec('start cmd /K "'+selected_command+'"');
+            process.exit(1);
+        } else if (args[0] == 'is') {
+            shell.exec('start '+obj.github+'/issues');
+            process.exit(1);
+        }
+    })    
 }
 
-jsonfile.readFile(file, function(err, obj) {
-    if (!obj) {
-        jsonfile.writeFileSync(file, config_default, {spaces: 2});
-        config = config_default;
-        fatihReady();
-    } else {
-        config = obj;
-        fatihReady();
-    }
-})
-
-class dbClass {
-    set(key, value) {
-        config[key] = value;
-        jsonfile.writeFileSync(file, config, {spaces: 2})
-    }
-
-    get(key) {
-       return config[key]; 
-    }
-}
-
-function fatihReady () {
-    db = new dbClass();
-    editor_path = db.get('editor');
-    projects = db.get('projects');
-    program.parse(process.argv);  
-}
-
-// PROGRAM BAŞLAR
-/////////////////
-/////////////////
-
-program.version('1.0.3');
+program.version('2.0.0');
 
 // ADD COMMAND
 program
-.command('add')
+.command('init')
 .description('yeni bir proje ekleyin')
 .action(function(env, options){
-    var questions = [
+    var init_questions = [
         {
             type: 'input',
             name: 'project_name',
-            message: 'Projenizin ismi ne olsun?',
+            message: 'Proje adı?'
+        },
+        {
+            type: 'input',
+            name: 'github_url',
+            message: 'Github URL?'
         }
     ];
 
-    inquirer.prompt(questions).then(function (answers) {
-        projects[answers.project_name] = {
-            "location": path_here
-        };              
-        db.set('projects',projects);             
+    inquirer.prompt(init_questions).then(function (answers) {
+        var project_array = {
+            name:answers.project_name,
+            github:answers.github_url,
+            custom_commands : {
+                "pull" : "git pull",
+                "push" : "git push"
+            }
+        };
+        jsonfile.writeFileSync('./fatih.json', project_array, {spaces: 2});       
     });
 });
 
-// ADD COMMAND
-program
-.command('config')
-.description('Config dosyasını düzenleyin')
-.action(function(env, options){
-    shell.exec('"'+editor_path+'" -n "'+file+'"');
-});
-
-// ADD COMMAND
-program
-.command('set [file_name]')
-.description('Editörünüzü seçin')
-.action(function(env, options){
-    if (env) {
-        var editor_path = path.resolve(process.cwd(), env);
-        db.set('editor',editor_path);        
-    } else {
-        console.log('Dosya seçmediniz');
-    }
-
-});
-
-// ADD COMMAND
-program
-.command('editor [project_name]')
-.description('Projenizi editörde açın')
-.action(function(env, options){
-    if (env) {
-        console.log(env);
-        if (!projects[env]) {
-            console.log("Project not found");
-            return;
-        }
-
-        var project_path = projects[env].location;
-        shell.exec('"'+editor_path+'" -n "'+project_path+'"');
-    } else {
-        var questions = [
-            {
-                type: 'list',
-                name: 'project',
-                message: 'Hangi projeyi editorde açacaksınız?',
-                choices: Object.keys(projects)
-            }
-        ];
-
-        inquirer.prompt(questions).then(function (answers) {
-            var project_path = projects[answers.project].location;
-            shell.exec('"'+editor_path+'" -n "'+project_path+'"');                
-        });
-    }
-});
-
-// ADD COMMAND
-program
-.command('terminal [project_name]')
-.description('Projenizi terminalde açın')
-.action(function(env, options){
-    if (env) {
-        console.log(env);
-        if (!projects[env]) {
-            console.log("Project not found");
-            return;
-        }
-
-        var project_path = projects[env].location;
-        shell.exec('start cmd /K "cd '+project_path+'"');
-    } else {
-        var questions = [
-            {
-                type: 'list',
-                name: 'project',
-                message: 'Hangi projeyi editorde açacaksınız?',
-                choices: Object.keys(projects)
-            }
-        ];
-
-        inquirer.prompt(questions).then(function (answers) {
-            var project_path = projects[answers.project].location;
-            shell.exec('start cmd /K "cd '+project_path+'"');                
-        });
-    }
-});
-
-// ADD COMMAND
-program
-.command('folder [project_name]')
-.description('Projenizi klasörde açın')
-.action(function(env, options){
-    if (env) {
-        console.log(env);
-        if (!projects[env]) {
-            console.log("Project not found");
-            return;
-        }
-
-        var project_path = projects[env].location;
-        shell.exec('start "" "'+project_path+'"');
-    } else {
-        var questions = [
-            {
-                type: 'list',
-                name: 'project',
-                message: 'Hangi projeyi klasörde açacaksınız?',
-                choices: Object.keys(projects)
-            }
-        ];
-
-        inquirer.prompt(questions).then(function (answers) {
-            var project_path = projects[answers.project].location;
-            shell.exec('start "" "'+project_path+'"');               
-        });
-    }
-});
+program.parse(process.argv);
